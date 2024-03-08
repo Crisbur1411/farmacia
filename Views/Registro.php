4c25bc5ff38d94f6a1e60214b560f1813eef4687
Crisbur1411 <crisbur141102@gmail.com>
2024-03-08 00:00:58 -0600

    S

--- Views/Registro.php
+++ Views/Registro.php
@@ -25,6 +25,7 @@ $conexion->cerrarConexion();
     <title>Registro</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="./Registro.css">
 </head>
 <body>
@@ -34,19 +35,21 @@ $conexion->cerrarConexion();
                 <span><i class="icon icon-user"></i></span>
             </div>
             <div class="form-group">
-                <input type="text" class="form-control item" id="Nombre" placeholder="Nombre(s)">
+                <input type="text" class="form-control item" id="Nombre" placeholder="Nombre(s)" required>
             </div>
             <div class="form-group">
-                <input type="text" class="form-control item" id="A_paterno" placeholder="Apellido paterno">
+                <input type="text" class="form-control item" id="A_paterno" placeholder="Apellido paterno" required>
             </div>
             <div class="form-group">
-                <input type="text" class="form-control item" id="A_materno" placeholder="Apellido materno">
+                <input type="text" class="form-control item" id="A_materno" placeholder="Apellido materno" required>
             </div>
             <div class="form-group">
-                <input type="text" class="form-control item" id="F_N" placeholder="Fecha de Nacimiento">
+                <!-- Input de fecha de nacimiento con selector -->
+                <input type="text" class="form-control item" id="F_N" placeholder="Fecha de Nacimiento" readonly>
             </div>
             <div class="form-group">
-                <input type="text" class="form-control item" id="Telefono" placeholder="Telefono">
+                <!-- Input de teléfono con validación de longitud y solo números -->
+                <input type="tel" class="form-control item" id="Telefono" placeholder="Telefono" maxlength="10" required>
             </div>
             <div class="form-group">
                 <input type="text" class="form-control item" id="Usuario" placeholder="Usuario">
@@ -62,7 +65,38 @@ $conexion->cerrarConexion();
         </div>
     </div>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <script src="./Registro.js"></script>
+    <script>
+        // Activar el selector de fecha
+        $( function() {
+            $( "#F_N" ).datepicker({
+                changeMonth: true,
+                changeYear: true,
+                yearRange: "-100:+0", // Rango de años permitidos
+                dateFormat: "dd/mm/yy", // Formato de fecha (día/mes/año)
+                onClose: function(selectedDate) {
+                    // Reducir el año a los dos últimos dígitos
+                    var dateParts = selectedDate.split('/');
+                    var year = dateParts[2].substring(2);
+                    $( "#F_N" ).val(dateParts[0] + '/' + dateParts[1] + '/' + year);
+                }
+            });
+        });
+
+        // Validar que los campos de nombre y apellidos solo contengan letras
+        $('#Nombre, #A_paterno, #A_materno').on('input', function() {
+            var sanitized = $(this).val().replace(/[^A-Za-zñÑáéíóúÁÉÍÓÚ\s]/g, '');
+            $(this).val(sanitized);
+        });
+
+        // Validar que el campo de teléfono solo acepte números y tenga una longitud de 10 dígitos
+        $('#Telefono').on('input', function() {
+            var sanitized = $(this).val().replace(/\D/g, '');
+            sanitized = sanitized.substring(0, 10);
+            $(this).val(sanitized);
+        });
+    </script>
 </body>
 </html>
